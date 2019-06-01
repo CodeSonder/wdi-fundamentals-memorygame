b614780ef6f45bbcc8b9daa9cf10e39172b37367
Jorge Esparza <corinthian@Jorges-MacBook-Air.local>
2019-06-01 15:29:03 -0400

    created game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,9 @@
+console.log("Up and Running!");
+var myName = "Jorge";
+myName = "Corinthian";
+var cardOne = "queen";
+var cardTwo = "queen";
+var cardThree = "king";
+var cardFour = "king";
+console.log("User flipped " + cardOne);
+console.log("User flipped " + cardTwo);
