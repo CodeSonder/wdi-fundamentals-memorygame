eb939914b494f96e8b4f3db06f0e781e721392b1
Jorge Esparza <corinthian@Jorges-MacBook-Air.local>
2019-06-05 19:58:18 -0400

    game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,9 +1,38 @@
+// Hello All My Name is Jorge AKA Corinthian
 console.log("Up and Running!");
 var myName = "Jorge";
 myName = "Corinthian";
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
+/* created an array of cards, created an undefined array for cards in play.
+created two variables for cards one and two. pushed both card one and two into cards in play array.
+set an if else statement with conditions that if the value of cards chosen is two and cards in play are equal set an alert to match other wise if not equal
+set an alert saying sorry try again*/
+var cards = ["Queen", "Queen", "King", "King"];
+ 
+var cardsInPlay = [];
+ 
+ function matchCheck() {
+    if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {
+ alert("Match!");
+ } else {
+ 	alert("Sorry, Try again");
+ }
+ };
+
+function flipCard(cardId) {
+	console.log("User flipped" + " " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+	matchCheck();
+
+
+};
+
+flipCard(0);
+
+
+
+
+
+
+
+ 
+   
