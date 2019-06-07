f52ccd8e051106898c9d8f9dc257e3563f64c7c2
Jorge Esparza <corinthian@Jorges-MacBook-Air.local>
2019-06-06 20:40:30 -0400

    logic3

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,61 +1,86 @@
-// Hello All My Name is Jorge AKA Corinthian
-console.log("Up and Running!");
-var myName = "Jorge";
-myName = "Corinthian";
-/* created an array of cards, created an undefined array for cards in play.
-created two variables for cards one and two. pushed both card one and two into cards in play array.
-set an if else statement with conditions that if the value of cards chosen is two and cards in play are equal set an alert to match other wise if not equal
-set an alert saying sorry try again*/
+console.log("Up and running!");
+var myName = "Jorge"
+myName = "Corinthian"
+
+/* create an object cards within an array, provide values for each item
+in the array*/
 var cards = [
-{
-	rank: "Queen",
-	suit: "Hearts",
-	cardImage: "images/queen-of-hearts.png"
-},
-{
-	rank: "Queen",
-	suit: "Diamonds",
-	cardImage: "images/queen-of-diamonds.png"
-},
-{
-	rank: "King",
-	suit: "Hearts",
-	cardImage: "images/king-of-hearts.png"
-},
-{
-	rank: "King",
-	suit: "Diamonds",
-	cardImage: "images/king-of-diamonds.png"
-}
+   {
+       rank: "queen",
+       suit: "hearts",
+       cardImage: "images/queen-of-hearts.png",
+   },
+   {
+      rank: "queen",
+      suit: "diamonds",
+      cardImage: "images/queen-of-diamonds.png",
+   },
+
+   {
+    rank: "king",
+    suit: "hearts",
+    cardImage: "images/king-of-hearts.png",
+   },
+    {
+    rank: "king",
+    suit: "diamonds",
+    cardImage: "images/king-of-diamonds.png",
+   },
 ];
- 
+// create an undefined array named cardsInPlay
 var cardsInPlay = [];
- 
- function matchCheck() {
-    if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {
- alert("Match!");
- } else {
- 	alert("Sorry, try again");
- }
+
+ function checkForMatch() {
+  /* if two cards are in play check to see if they match, if yes alert
+  user and say Match!. if not alert user saying sorry try again.*/
+    if (cardsInPlay[0] === cardsInPlay[1]) {
+          alert("Match!");
+         
+
+      } else {
+          alert("Sorry, try again");
+        };
+
+  };
+
+  //create a function named flipCard with no parameters
+function flipCard() {
+  //variable cardId is the vallue of data-id
+    var cardId = this.getAttribute('data-id');
+    //add cards to cardsInPlay array by rank and cardId
+  cardsInPlay.push(cards[cardId].rank);
+  //card image comes from CardElement
+ this.setAttribute('src', cards[cardId].cardImage);
+// if cards in play array is two check for match
+ if (cardsInPlay.length === 2){
+    checkForMatch();
+    
+
+ };
 };
-function flipCard(cardId) {
-	console.log("User flipped" + " " + cards[cardId].rank);
-	cardsInPlay.push(cards[cardId].rank);
-	matchCheck();
-	
-	
 
+console.log(cardsInPlay);
 
+//create a function named createBoard with no parameters
+ function createBoard() {
+ //create a variable cardTable assign it name of game-board
+  var cardTable = document.getElementById('game-board');
+  //run a loop through cards
+  for (var i = 0; i < cards.length; i++) {
+    //create cardElement variable with img tag
+  var cardElement = document.createElement('img');
+  //set attribute for cardElement src with value images/back.png
+    cardElement.setAttribute('src', 'images/back.png');
+    //set attribute data-id with value i
+    cardElement.setAttribute('data-id', i);
+    /*add an event listener, listening to click and function to do after
+    click*/
+    cardElement.addEventListener('click', flipCard);
+    //add cardElement to parent element cardTable
+    cardTable.appendChild(cardElement);
+
+  }
 };
+//call on the createBoard function
+createBoard();
 
-flipCard(1);
-
-
-
-
-
-
-
-
- 
-   
